dbb199d259a4549131cdd501edd9722a3452ec93
roman.shvets <roman.shvets.1989@gmail.com>
2022-09-09 05:05:27 +0300

    fixes

---
+++ problem_002/java/src/main/java/com/rshvets/euler/Problem002.java
@@ -0,0 +1,12 @@
+package com.rshvets.euler;
+
+public class Problem002 {
+
+    public static void main(String[] args) {
+        System.out.printf("Result: %s", sumOfEvenFibonacciNumbers(1000L));
+    }
+
+    public static long sumOfEvenFibonacciNumbers(long upperBound) {
+        return 0L;
+    }
+}
