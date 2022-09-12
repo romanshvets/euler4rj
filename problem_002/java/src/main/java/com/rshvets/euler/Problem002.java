a7e7f156a6fa8596f1e91c31ef73f12e9374cc09
Roman Shvets <roman.shvets.1989@gmail.com>
2022-09-12 14:53:13 +0300

    solving problem 002

--- problem_002/java/src/main/java/com/rshvets/euler/Problem002.java
+++ problem_002/java/src/main/java/com/rshvets/euler/Problem002.java
@@ -3,10 +3,28 @@ package com.rshvets.euler;
 public class Problem002 {
 
     public static void main(String[] args) {
-        System.out.printf("Result: %s", sumOfEvenFibonacciNumbers(1000L));
+        System.out.printf("Result: %s", sumOfEvenFibonacciNumbers(4_000_000L));
     }
 
     public static long sumOfEvenFibonacciNumbers(long upperBound) {
-        return 0L;
+        var a = 1;
+        var b = 2;
+
+        var c = 0;
+
+        var sum = b;
+
+        while (c <= upperBound) {
+            c = a + b;
+
+            if (c % 2 == 0)
+                sum += c;
+
+            a = b;
+            b = c;
+        }
+
+        return sum;
     }
+
 }
