4d2d4223bccc8f57f8b1c8a251ed393c0d479f5e
roman.shvets <roman.shvets.1989@gmail.com>
2022-09-13 11:17:05 +0300

    problem 003

--- problem_003/java/src/main/java/com/rshvets/euler/Problem003.java
+++ problem_003/java/src/main/java/com/rshvets/euler/Problem003.java
@@ -1,25 +1,23 @@
 package com.rshvets.euler;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Problem003 {
 
-    /*The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?*/
-
-
-
     public static void main(String[] args) {
-        System.out.println("hello owrld");
+        System.out.printf("Result: %s", largestPrimeFactor(600851475143L));
     }
 
     public static long largestPrimeFactor(long n) {
         return 0;
     }
 
-    public static List<Long> primeFactors()
+    public static List<Long> primeFactors(long n) {
+        return Collections.emptyList();
+    }
 
-
-
+    public static boolean isPrime(long n) {
+        return false;
+    }
 }
