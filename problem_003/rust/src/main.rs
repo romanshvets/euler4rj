9bea41fee1dbed93e3e81f69d656f1efbd268e8e
roman.shvets <roman.shvets.1989@gmail.com>
2022-09-22 16:46:15 +0300

    p003

--- problem_003/rust/src/main.rs
+++ problem_003/rust/src/main.rs
@@ -1,14 +1,12 @@
 fn main() {
-    let prime_factors = get_prime_factors(600851475143);
-
-    println!("Result: {}", get_largest_prime_factor(&prime_factors));
+    println!("Result: {}", get_largest_prime_factor(600851475143));
 }
 
-fn get_prime_factors(mut n: u64) -> Vec<u64> {
-    let mut result = vec![];
+fn get_largest_prime_factor(mut n: u64) -> u64 {
+    let mut max_prime_factor = 0;
 
     while n % 2 == 0 {
-        add_if_unique(&mut result, 2);
+        max_prime_factor = 2;
 
         n = n / 2;
     }
@@ -17,65 +15,43 @@ fn get_prime_factors(mut n: u64) -> Vec<u64> {
 
     for i in (3..=upper_bound).step_by(2) {
         while n % i == 0 {
-            add_if_unique(&mut result, i);
+            max_prime_factor = i;
 
             n = n / i;
         }
     }
 
     if n > 2 {
-        add_if_unique(&mut result, n);
+        max_prime_factor = n;
     }
 
-    result
-}
-
-fn add_if_unique(array: &mut Vec::<u64>, value: u64) -> () {
-    if !array.contains(&value) {
-        array.push(value);
-    }
-}
-
-fn get_largest_prime_factor(prime_factors: &[u64]) -> u64 {
-    if prime_factors.is_empty() {
-        return 0;
-    }
-
-    prime_factors[prime_factors.len() - 1]
+    max_prime_factor
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_largest_prime_factor, get_prime_factors};
+    use crate::get_largest_prime_factor;
     use std::time::Instant;
 
     #[test]
     fn test_get_largest_prime_factor() {
-        assert_eq!(7, get_largest_prime_factor(&vec![2, 7]));
-        assert_eq!(29, get_largest_prime_factor(&vec![5, 7, 13, 29]));
-        assert_eq!(997, get_largest_prime_factor(&vec![2, 53, 997]));
-    }
-
-    #[test]
-    fn test_prime_factors() {
-        assert_eq!(vec![2, 7], get_prime_factors(392));
-        assert_eq!(vec![5, 7, 13, 29], get_prime_factors(13195));
-        assert_eq!(vec![2, 53, 997], get_prime_factors(105682));
+        assert_eq!(7, get_largest_prime_factor(392));
+        assert_eq!(29, get_largest_prime_factor(13195));
+        assert_eq!(997, get_largest_prime_factor(105682));
     }
 
     #[test]
     fn test_prime_factors_performance() {
-        let upper_bounds = vec![10_000, 100_000, 1_000_000, 10_000_000];
+        let now = Instant::now();
 
-        for upper_bound in upper_bounds {
-            let now = Instant::now();
-
-            for n in 2..=upper_bound {
-                let _prime_factors = get_prime_factors(n);
-            }
-
-            let elapsed = now.elapsed();
-            println!("Upper bound = {}, Duration = {:?}", upper_bound, elapsed);
+        for n in 2..=1_000_000 {
+            let _max_prime_factors = get_largest_prime_factor(n);
         }
+
+        let elapsed = now.elapsed();
+        println!(
+            "Calculated prime factors for each number within [2, 1000000] range in {:?}",
+            elapsed
+        );
     }
 }
